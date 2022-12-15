6886ee5cea60662dd5a3919117d8442e8a8aca48
Emerzon Javier Kolki Martinez <emerzonkolki@gmail.com>
2022-12-15 15:49:44 -0500

    fix(authroute.ts): add account repository in register route

--- src/user/infrastructure/route/authRoute.ts
+++ src/user/infrastructure/route/authRoute.ts
@@ -3,11 +3,16 @@ import { Router } from 'express'
 import { PostgreSQLUserRepository } from '../repository/PostgreSQLUserRepository'
 import { AuthUseCase } from '../../application/AuthUseCase'
 import { AuthController } from '../controllers/AuthController'
+import { PostgreSQLAccountRepository } from '../../../account/infrastructure/repository/PostgreSQLAccountRepository'
 
 const router = Router()
 
 const postgreSQLUserRepository = new PostgreSQLUserRepository()
-const authUseCase = new AuthUseCase(postgreSQLUserRepository)
+const postgreSQLAccountRepository = new PostgreSQLAccountRepository()
+const authUseCase = new AuthUseCase(
+  postgreSQLUserRepository,
+  postgreSQLAccountRepository
+)
 const authController = new AuthController(authUseCase)
 
 router.post('/signUp', authController.postRegisterUser)
